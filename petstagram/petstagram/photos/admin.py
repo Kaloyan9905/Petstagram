581935f3242fbf7273288d84dfa1497b07c1fd84
kaloyan <milchevkaloian@gmail.com>
2024-01-01 17:11:31 +0200

    workshop part 1

---
+++ petstagram/petstagram/photos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from petstagram.photos.models import Photo
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_of_publication', 'description', 'get_tagged_pets')
+
+    @staticmethod
+    def get_tagged_pets(obj):
+        return ', '.join([pet.name for pet in obj.tagged_pets.all()])
+
+
